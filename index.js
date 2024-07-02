b637412a5ecdac48be2397c86df9a432488fd16a
ESTON NDUNGU <estonndungu1@gmail.com>
2024-07-02 17:02:33 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,20 @@
+//
+function receivesAFunction(callback) { // takes the argument callback
+    //Calls the call back function
+    callback() 
+}
+
+
+function returnsANamedFunction() {
+    function namedFunction() {  // define a namedFunction
+        console.log('This is a named function.')
+    }
+    return namedFunction //returns the namedFunction
+
+    }
+
+    function returnsAnAnonymousFunction() { // takes no arguments
+        return function () { // return anonymous function directly without defining it first
+            console.log('This is an anonymous function')
+        }
+    }
